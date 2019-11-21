c5791ba80cff1c59459681366b399d00c6586ee8
feresCr7 <feressayari5@gmail.com>
2019-11-21 15:29:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 0,
+      start: false
+    };
+    setInterval(
+      () =>
+        this.state.start
+          ? this.setState({
+              time: this.state.time + 1000
+            })
+          : clearInterval(this.state.time),
+      1000
+    );
+  }
+  startCounting = () => {
+    this.setState({
+      start: !this.state.start
+    });
+  };
+  render() {
+    const hours = Math.floor((this.state.time / 3600000) % 24);
+    const minutes = Math.floor((this.state.time / 60000) % 60);
+    const seconds = Math.floor((this.state.time / 1000) % 60);
+    return (
+      <div className="App">
+        <h1>
+          {String(hours).padStart(2, "0")} : {String(minutes).padStart(2, "0")}{" "}
+          : {String(seconds).padStart(2, "0")}
+        </h1>
+        <button onClick={this.startCounting}>
+          {this.state.start ? "Pause" : "Start"}
+        </button>
+        <button onClick={() => this.setState({ time: 0, start: false })}>
+          Reset
+        </button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App ;
